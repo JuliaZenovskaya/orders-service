3f34860983df8b271397e4bc76214e8db04f3582
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-10-09 00:32:39 +0300

    Initial commit

---
+++ orders_application/src/main/java/com/microservices/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.microservices.service;
+
+import com.microservices.model.AddItem;
+import com.microservices.model.Order;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface OrderService {
+    ArrayList<Order> getAllOrders() throws SQLException;
+    Order getOrderById(int id) throws SQLException;
+    void changeOrderStatus(int id, String status) throws SQLException;
+    void addItemToOrder(AddItem addItem) throws SQLException;
+    void addAddressToOrder(int orderId, String email, String country, String city, String street, int house, int corp, int flat) throws SQLException;
+}
---
+++ orders_application/src/main/java/com/microservices/service/impl/OrderServiceImpl.java
@@ -0,0 +1,41 @@
+package com.microservices.service.impl;
+
+import com.microservices.model.AddItem;
+import com.microservices.model.Order;
+import com.microservices.database.DBHelper;
+import com.microservices.service.OrderService;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    private DBHelper dbHelper = new DBHelper();
+
+    @Override
+    public ArrayList<Order> getAllOrders() throws SQLException {
+        return dbHelper.getAllOrders();
+    }
+
+    @Override
+    public Order getOrderById(int id) throws SQLException {
+        return dbHelper.getOrderById(id);
+    }
+
+    @Override
+    public void changeOrderStatus(int id, String status) throws SQLException {
+        dbHelper.changeOrderStatus(id, status);
+    }
+
+    @Override
+    public void addItemToOrder(AddItem addItem) throws SQLException {
+        dbHelper.addItemToOrder(addItem);
+    }
+
+    @Override
+    public void addAddressToOrder(int orderId, String email, String country, String city, String street,
+                                  int house, int corp, int flat) throws SQLException {
+        dbHelper.addInfoToOrder(orderId, email, country, city, street, house, corp, flat);
+    }
+}
